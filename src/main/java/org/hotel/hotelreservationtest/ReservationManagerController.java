8972e26cca20d2b98046f4d0da4eb1651c930d07
Roee Palmon <new.roeepalmon@gmail.com>
2024-11-18 12:26:57 -0800

    add options for status and ui change

--- src/main/java/org/hotel/hotelreservationtest/ReservationManagerController.java
+++ src/main/java/org/hotel/hotelreservationtest/ReservationManagerController.java
@@ -3,14 +3,24 @@ package org.hotel.hotelreservationtest;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.*;
-import javafx.scene.control.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 
 public class ReservationManagerController extends BaseController {
     @FXML
     private Button back; // Assuming fx:id of the Button is "back"
 
+    @FXML
+    private ChoiceBox<String> reservationStatus; // Assuming fx:id of the ChoiceBox is "reservationStatus"
 
+    //function to return to control panel
+
+    @FXML
+    public void initialize() {
+        // Populate the ChoiceBox with status options
+        reservationStatus.getItems().addAll("booked", "checked_in", "checked_out", "cancelled");
+    }
 
     //function to return to control panel
     @FXML
